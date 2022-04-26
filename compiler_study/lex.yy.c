cfc12b57b11a746cbd3ebe012ade4d7775a0011c
HyunSwim <coldsz@naver.com>
2022-04-27 05:33:14 +0900

    complete compiler first assignment

--- compiler_study/lex.yy.c
+++ compiler_study/lex.yy.c
@@ -354,8 +354,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -363,9 +363,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[9] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    3,    2,    1,    0,    1,    0
+        0,    0,    6,    4,    2,    3,    1,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -374,8 +374,8 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    3,    1,    4,    1,    1,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -400,29 +400,31 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[3] =
+static const YY_CHAR yy_meta[6] =
     {   0,
-        1,    1
+        1,    1,    1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[9] =
+static const flex_int16_t yy_base[11] =
     {   0,
-        0,    0,    5,    2,    6,    0,    6,    6
+        0,    0,    7,    8,    8,    8,    0,    0,    8,    4
     } ;
 
-static const flex_int16_t yy_def[9] =
+static const flex_int16_t yy_def[11] =
     {   0,
-        8,    1,    8,    8,    8,    4,    8,    0
+        9,    1,    9,    9,    9,    9,   10,   10,    0,    9
     } ;
 
-static const flex_int16_t yy_nxt[9] =
+static const flex_int16_t yy_nxt[14] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8
+        4,    4,    5,    6,    7,    8,    9,    3,    9,    9,
+        9,    9,    9
     } ;
 
-static const flex_int16_t yy_chk[9] =
+static const flex_int16_t yy_chk[14] =
     {   0,
-        1,    1,    4,    4,    3,    8,    8,    8
+        1,    1,    1,    1,    1,   10,    3,    9,    9,    9,
+        9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,12 +441,15 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "linenum.l"
-#line 2 "linenum.l"
-#include<stdio.h>
-int lineno = 1;
-#line 447 "lex.yy.c"
-#line 448 "lex.yy.c"
+#line 1 "compiler1.l"
+#line 2 "compiler1.l"
+    #include <stdio.h>
+    #define NUMBER 256
+    #define PLUS 255
+    #define MINUS 254
+#line 451 "lex.yy.c"
+/*regexp definitions */
+#line 453 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -661,9 +666,9 @@ YY_DECL
 		}
 
 	{
-#line 8 "linenum.l"
+#line 11 "compiler1.l"
 
-#line 667 "lex.yy.c"
+#line 672 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -690,13 +695,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 8 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -721,17 +726,32 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 9 "linenum.l"
-{printf ("%5d %s", lineno++, yytext);}
+#line 12 "compiler1.l"
+{return NUMBER;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 10 "linenum.l"
+#line 13 "compiler1.l"
+return PLUS;
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 14 "compiler1.l"
+return MINUS;
+	YY_BREAK
+case 4:
+/* rule 4 can match eol */
+YY_RULE_SETUP
+#line 15 "compiler1.l"
+{ }
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 16 "compiler1.l"
 ECHO;
 	YY_BREAK
-#line 735 "lex.yy.c"
+#line 755 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1028,7 +1048,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1056,11 +1076,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1736,7 +1756,74 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "linenum.l"
+#line 16 "compiler1.l"
 
-int main()
-{while(1){yylex();} return 0;}
+
+int
+main(){
+    int token;
+    char* one;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    char ch;
+    while((token=yylex())){
+        switch(token){
+            case NUMBER:
+                if(k==0)
+                    k = 1;
+                else{
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            case PLUS:
+                if(k==1)
+                    k = 0;
+                else{
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            case MINUS:
+                if(k==1)
+                    k = 0;
+                else{
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            default:
+                printf("Error\n");
+                return 0;
+        }   
+    }
+    while((token=yylex())){
+        switch(token){
+            case NUMBER:
+                if(i == 0){
+                    printf("%s ",yytext);
+                    i = 1;
+                }
+                else{
+                    printf("%s %s ",yytext,one);
+                    j = 0;
+                }
+                break;
+            case PLUS:
+                if(j==1)
+                    printf("Error\n");
+                one = "+";
+                j = 1;
+                break;
+            case MINUS:
+                if(j==1)
+                    printf("Error\n");
+                one = "-";
+                j = 1;
+                break;
+            default: printf("Error\n");
+        }   
+    }
+    printf("\n");
+}
